43ad72e275f1e449f36290988d3b12cee67add3b
alextidd <at31@sanger.ac.uk>
2024-04-29 15:16:57 +0100

    move metadata wrangling to R

---
+++ src/00_wrangle_metadata.R
@@ -0,0 +1,37 @@
+# dirs
+wd <- "/lustre/scratch126/casm/team154pc/at31/chemo_trees/"
+canapps_dir <- "/nfs/cancer_ref01/nst_links/live/"
+setwd(wd)
+
+# libraries
+library(magrittr)
+
+# get files from donor metadata
+donor_metadata <-
+    readr::read_tsv("data/donor_metadata.tsv")
+sample_sheet <-
+    donor_metadata %>%
+    purrr::pmap(function(donor_id, donor_id_long, project_id, experiment_id, donor_age) {
+        list.files(
+            file.path(canapps_dir, project_id),
+            pattern = donor_id, 
+            full.names = T) %>%
+            tibble::enframe(value = "sample_dir") %>%
+            dplyr::transmute(
+                donor_id = donor_id,
+                sample_id = basename(sample_dir),
+                project_id = project_id,
+                bam = file.path(sample_dir, paste0(sample_id, ".sample.dupmarked.bam")),
+                pindel_vcf = file.path(sample_dir, paste0(sample_id, ".pindel.annot.vcf.gz")),
+                caveman_vcf = file.path(sample_dir, paste0(sample_id, ".caveman_c.annot.vcf.gz"))
+            ) %>%
+            # check if the files exist
+            dplyr::mutate(
+                dplyr::across(c('bam', 'pindel_vcf', 'caveman_vcf'),
+                ~ ifelse(file.exists(.), ., NA)))
+    }) %>%
+    dplyr::bind_rows()
+
+# write sample sheet
+sample_sheet %>%
+    readr::write_csv("data/sample_sheet.csv")
--- src/run.sh
+++ src/run.sh
@@ -5,12 +5,9 @@
 wd=/lustre/scratch126/casm/team154pc/at31/chemo_trees/
 cd $wd
 
-# load singularity
-module load singularity
-
 # run the workflow 
 nextflow run nf-chemo-trees/ \
     --sample_sheet data/sample_sheet.csv \
     --outdir out \
     -c /lustre/scratch125/casm/team268im/at31/RA_som_mut/scomatic/config/LSF.config \
-    -resume
+    -resume 
