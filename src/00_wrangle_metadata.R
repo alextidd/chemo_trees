7e2b6ab07fc2bf371964b2da9bcaf2d20e8bfd98
alextidd <at31@sanger.ac.uk>
2024-05-21 16:56:42 +0100

    implement nf-schema

--- src/00_wrangle_metadata.R
+++ src/00_wrangle_metadata.R
@@ -20,8 +20,6 @@ sample_sheet <-
             dplyr::transmute(
                 donor_id = donor_id,
                 sample_id = basename(sample_dir),
-                project_id = project_id,
-                experiment_id = experiment_id,
                 bam = file.path(sample_dir, paste0(sample_id, ".sample.dupmarked.bam")),
                 pindel_vcf = file.path(sample_dir, paste0(sample_id, ".pindel.annot.vcf.gz")),
                 caveman_vcf = file.path(sample_dir, paste0(sample_id, ".caveman_c.annot.vcf.gz"))
